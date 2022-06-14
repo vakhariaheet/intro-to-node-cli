d95bbf922545db65077600152a4d3df79e82f602
Heet Vakharia <heetkv@gmail.com>
2022-06-14 17:37:55 +0530

    Removing Unwanted Files

--- dist/utils/utils.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import { RunCommandOptions } from '../types';
-export declare const showLoading: (prefix?: string) => NodeJS.Timer;
-export declare const runCommand: (command: string | RunCommandOptions) => Promise<unknown>;
-export declare const readFile: (file: string) => Promise<string>;
